a2483c29f832c6f550912ff9d1863412a56da542
chris <ChristopherDeveloper3@gmail.com>
2024-09-16 00:27:04 -0600

    Add project files.

---
+++ BasicChatRoomSignalR/Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicChatRoomSignalR.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>() {
+                { 1, "Gaming" },
+                { 2, "Programming"},
+                { 3, "Series"}
+        };
+
+        public IActionResult Index()
+        {
+            Rooms.GetValueOrDefault(0);
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
